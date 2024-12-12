015c0c1113ba3c60932cf1c8f664073ca764f630
gledson-snts-ghub <gledson.snts.workspace@gmail.com>
2024-12-11 22:13:39 -0300

    fix(style): responsivity

--- app/pages/dashboard.tsx
+++ app/pages/dashboard.tsx
@@ -2,7 +2,7 @@ import { TimeTracking } from "../components/time-tracking";
 
 export default function Dashboard() {
     return (
-        <div className="max-h-fit h-full flex justify-center bg-gradient-to-b from-blueDark to-dark px-8">
+        <div className="max-h-screen h-screen flex justify-center bg-gradient-to-b from-blueDark to-dark px-8">
             <TimeTracking />
         </div>
     );
